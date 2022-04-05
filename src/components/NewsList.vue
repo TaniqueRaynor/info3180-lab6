e96ab41e50cd8f464d4f5f35574880a79d961b84
Tanique Raynor <tanique.raynor@gmail.com>
2022-04-04 20:11:28 -0500

    lab6

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,7 +1,21 @@
 <template>    
-    <ul class="news__list">      
-        <li v-for="article in articles" class="news__item">{{ article.title }}</li>  
-    </ul>
+    <div class="news__list">      
+        <div v-for="article in articles" class="news__item">
+            <img class="card-img-top" :src= article.UrlToImage alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">{{ article.title }}</h4>
+                <p class="card-text">{{ article.description}}</p>
+            </div>
+        </div>  
+    </div>
+     <form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">       
+        <div class="input-group mx-sm-3 mb-2">         
+            <label class="visually-hidden" for="search">Search</ label>         
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />        
+            <button class="btn btn-primary mb-2">Search</button>       
+        </div>       
+        <p>You are searching for {{ searchTerm }}</p>     
+     </form> 
 </template> 
 
 <script> 
@@ -9,18 +23,21 @@ export default {
     data() {     
         return {
             articles: []
+            searchTerm: '' 
         };   
     }, 
-    created() {       
-        let self =this;  
-        fetch('https://newsapi.org/v2/top-headlines?country=us', 
-{  
+    methods: {       
+        searchNews() {         
+            let self = this;        
+
+            fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en', { 
          headers: {         
             'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`     
          }     
     })          
                 .then(function(response) {             
                     return response.json();           
+
                  })           
                 .then(function(data) {            
                      console.log(data);    
